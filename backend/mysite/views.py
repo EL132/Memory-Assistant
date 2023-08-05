2fb614ccdae8869ab11a7c669cd57de363fd608c
fevinfelix <fevinfelix@gmail.com>
2023-08-05 16:06:49 -0400

    Update prompt format and move imageStorage.py
    Prompt was previously making LLM return the detected topics and used files in a '$' separated format near the end. The new prompt creates a separate identifying string for both topics and files detected.
    
    imageStorage.py was not able to be imported within views.py due to it being within mySite folder. It is now in the mySite folder.

--- backend/mysite/views.py
+++ backend/mysite/views.py
@@ -13,26 +13,30 @@ import os
 import openai
 from llama_index import SimpleDirectoryReader, GPTVectorStoreIndex
 
-openai.api_key = "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"
+openai.api_key = ""
 #openai.api_key = os.getenv("OPENAI_API_KEY") # THIS DOES NOT WORK
 documents = SimpleDirectoryReader('./mysite/training_data').load_data()
 index = GPTVectorStoreIndex(documents)
 query_engine = index.as_query_engine()
+topics = "Hashmaps, Trees, Linked Lists, Arrays, Strings, Stacks, Queues, Heaps"
 
 # access the LLM, passing in the user input
 def process_input(input):
     # do some processing here
-    ret = (query_engine.query(input + " In addition, please also print out which of the following topics this question falls under:\
-            Hashmaps, Trees, Linked Lists, Arrays, Strings, Stacks, Queues, Heaps." + 
-            " You can print out more than one topic if the question falls under more than one of the mentioned topics. " + 
+    ret = (query_engine.query(input + f"In addition, please also print out which of the following topics this question falls under:\{topics}." + 
+            "You should print out more than one topic if the question falls under more than one of the mentioned topics." + 
             "You must also print out the name or names of the files that you used to come up with your response. If no source files can be\
-                identified, then say 'No Source File Identified'." + 
-            "For the topic of the question and the name or names of relevant files, please start those responses with a $ and separate the two responses with a $."))
+                identified, then say 'No Source File Identified'. You should print out more than one file name if the answer you came up with\
+                     pulled information from more than one file." + 
+            "For the topic of the question, please start that response with 'Topic(s):' and return the topic or topics separated by commas.\
+                  For example, 'heaps, trees, stacks' or 'Queues' would both be valid responses to have after 'Topic(s):'." +
+            "For the name or names of relevant files, please start that response with 'File(s)' and follow the same comma separated approach as for 'Topic(s):'." +
+            "Put the response to the question first, then the 'File(s)', and finally the 'Topic(s)'."))
+    # prompt currently isn't returning all used file names. It only returned the Hashmap_notes.pdf when asked a question about trees and hashmaps combined.
     # ret = input + " testing without LLM query engine"
     return ret
 
-
-
+#-----------------------------------------------------------API Requests----------------------------------------------------------------------
 
 # get function for the images for the React frontend
 @csrf_exempt
@@ -90,11 +94,3 @@ def post(request: HttpRequest):
 
     print(processed_data)
     return JsonResponse(data)
-
-
-
-# decode LLM output
-def decode_output(output_str):
-    # Encode the output string to interpret escape sequences
-    encoded_str = output_str.encode().decode('unicode_escape')
-    return encoded_str
