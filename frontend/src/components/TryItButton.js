eb496eb930782f19936cae4d67d538bdabc224b9
Fevin Felix <87610636+FevinFelix@users.noreply.github.com>
2023-08-25 20:21:38 -0400

    Try It Yourself button reroutes to a page to input question

--- frontend/src/components/TryItButton.js
+++ frontend/src/components/TryItButton.js
@@ -1,9 +1,16 @@
 import './TryItButton.css';
+import React from 'react';
+import { useNavigate } from 'react-router-dom'
 
 const TryItButton = () => {
+    const navigate = useNavigate();
+    const redirectToOtherPage = () => {
+        navigate('/try-it'); // This will navigate to the '/try-it' route
+      };
+
     return (
         <div class="try-it-container">
-            <button class="try-it-button" type="button">Try It Yourself!</button>
+            <button class="try-it-button" type="button" onClick={redirectToOtherPage}>Try It Yourself!</button>
         </div>
     );
 };
