fa4079c82c93d67d78d2f7f8b61d66706e445ec0
Fevin Felix <87610636+FevinFelix@users.noreply.github.com>
2023-08-27 21:03:50 -0400

    Add delay to automatic page scroll

--- frontend/src/components/ScrollToTop.js
+++ frontend/src/components/ScrollToTop.js
@@ -1,14 +1,27 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const ScrollToTop = () => {
     // Extracts pathname property(key) from an object
     const { pathname } = useLocation();
+    const delay = 700;
+    const [scrollTimeout, setScrollTimeout] = useState(null);
   
     // Automatically scrolls to top whenever pathname changes
     useEffect(() => {
-      window.scrollTo(0, 0);
-    }, [pathname]);
+          // Clear the previous timeout if it exists
+        if (scrollTimeout) {
+            clearTimeout(scrollTimeout);
+        }
+  
+        // Create a new timeout to scroll after the specified delay
+        const newTimeout = setTimeout(() => {
+            window.scrollTo(0, 0);
+        }, delay);
+  
+        // Set the new timeout in state
+        setScrollTimeout(newTimeout);
+    }, [pathname, delay, scrollTimeout]); 
   }
   
   export default ScrollToTop;
