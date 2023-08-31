5be2bbb1dfa609927897e07b75c79681d7458af2
Fevin Felix <87610636+FevinFelix@users.noreply.github.com>
2023-08-31 19:20:54 -0400

    Remove footer components and add new documentation section in footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,7 +4,27 @@ import './Footer.css'; // Import the associated CSS file for styling
 const Footer = () => {
     return (
         <footer className="footer">
-            <div className="footer-content">
+
+            <div class="footer-container">
+                <div class="documentation-container">
+                    <div class="doc-image">
+                        <img src="./assets/documentation-icon.png" alt="" class="doc-png"></img>
+                    </div>
+                    <div class="doc-link">
+                        <h3 class="doc-text"> <a href="https://docs.google.com/document/d/1gYWHpejV6yz6YuLOveEU1rSg2IGPG1xlvesaJoj1Pnw/edit?usp=sharing">Documentation</a></h3> 
+                    </div>
+                </div>
+                <div class="elias-content">
+
+                </div>
+                <div class="fevin-content">
+
+                </div>
+            </div>
+
+
+
+            {/* <div className="footer-content">
                 <div className="top-footer">
                     <div className="footer-element">
                         <h3> <a href="https://el132.github.io/PersonalWebsite/">Elias Lind</a></h3>
@@ -20,7 +40,7 @@ const Footer = () => {
                     <p>&copy; {new Date().getFullYear()} Memory Assistant. All rights reserved.</p>
                 </div>
                 
-            </div>
+            </div> */}
         </footer>
     );
 };
