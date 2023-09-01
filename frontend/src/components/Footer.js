36d2484f9fa0f5f37be59124d8c8cc82735acef7
Fevin Felix <87610636+FevinFelix@users.noreply.github.com>
2023-08-31 20:12:34 -0400

    Add all components for Elias in new footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -15,10 +15,28 @@ const Footer = () => {
                     </div>
                 </div>
                 <div class="elias-content">
-
+                    <h3 class="elias name">Elias</h3>
+                    <div class="link-container">
+                        <div class="website-image">
+                            <img src="./assets/website.png" alt="" class="icon"></img>
+                        </div>
+                        <div class="website-link"><a href="https://el132.github.io/PersonalWebsite/">Personal Website</a></div>
+                    </div>
+                    <div class="link-container">
+                        <div class="linkedin-image">
+                            <img src="./assets/linkedin.png" alt="" class="icon"></img>
+                        </div>
+                        <div class="linkedin-link"><a href="https://www.linkedin.com/in/elias-lind-431546221/">LinkedIn</a></div>
+                    </div>
+                    <div class="link-container">
+                        <div class="github-image">
+                            <img src="./assets/github.png" alt="" class="icon"></img>
+                        </div>
+                        <div class="github-link"><a href="https://github.com/EL132">GitHub</a></div>
+                    </div>
                 </div>
                 <div class="fevin-content">
-
+                    <h3 class="fevin name">Fevin</h3>
                 </div>
             </div>
 
