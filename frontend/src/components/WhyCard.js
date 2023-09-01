e0d64fd60ed014e97f7ae2d9e24c59274475263f
Fevin Felix <87610636+FevinFelix@users.noreply.github.com>
2023-08-31 20:17:05 -0400

    Fix typo in Why text

--- frontend/src/components/WhyCard.js
+++ frontend/src/components/WhyCard.js
@@ -7,7 +7,7 @@ const WhyCard = () => {
                 <p className="why">Why?</p>
                 <p className="why-text">
                     After <span className="colored-word">countless</span> hours of technical interview preparation, we found ourselves
-                    <span className="colored-word">unable to remember</span> specific shortcuts, memorization techniques, and other concepts.
+                    <span className="colored-word"> unable to remember</span> specific shortcuts, memorization techniques, and other concepts.
                     As such, we decided to not leave these <span className="colored-word">gaps in our knowledge</span> up to combing through
                     our seemingly endless notes and instead build a <span className="colored-word">personalized assistant </span>
                         tailored for our notes. 
