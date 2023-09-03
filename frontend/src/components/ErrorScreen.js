3a2b6946d4ce855b669d845e01a103878b94a971
Fevin Felix <87610636+FevinFelix@users.noreply.github.com>
2023-09-03 14:44:12 -0400

    Add attribution for Storyset

--- frontend/src/components/ErrorScreen.js
+++ frontend/src/components/ErrorScreen.js
@@ -10,8 +10,10 @@ const ErrorScreen = () => {
             <div className="error-screen-container">
                 <div className="error-container">
                     <img src="./assets/error.svg" alt="" className="error-img"></img>
-                    <p style={{ color: 'black', fontSize: '1.2em'}}>Looks like there are too many users testing the site right now 😮‍💨</p>
-                    <p style={{ color: 'black', fontSize: '.7em'}}>Or our API key has been abused to obvilion...</p>
+                    <p style={{ color: 'black', fontSize: '.5em', marginBottom: '1vh'}}>Image from Storyset</p>
+                    <p style={{ color: 'black', fontSize: '1.4em'}}>Looks like there are too many users testing the site right now 😮‍💨</p>
+                    <p style={{ color: 'black', fontSize: '.9em'}}>Or our API key has been abused to obvilion...</p>
+                    
                 </div> 
             </div>
         </div>
